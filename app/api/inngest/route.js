c3abfc2b1951b805784d7596c821e73065754c97
Sagarnil Das <sagarnildass@gmail.com>
2025-05-09 03:04:47 +0530

    added remaining inngest functions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,19 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts } from "@/lib/inngest/function";
-
+import {
+  checkBudgetAlerts,
+  generateMonthlyReports,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    checkBudgetAlerts
+    processRecurringTransaction,
+    triggerRecurringTransactions,
+    generateMonthlyReports,
+    checkBudgetAlerts,
   ],
 });
